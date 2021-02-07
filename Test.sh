aeb9f2d2fd12c6105db690484658a9c9e36d196d
Pranav Murali <pranavmurali2001@gmail.com>
2021-02-07 22:05:18 +0530

    Update Test.sh

--- Test.sh
+++ Test.sh
@@ -4,7 +4,7 @@ url="https://api.github.com/"
 
 
 #curl -i $url"users/PranavMurali"
-token="f0fd9acfd681d61170e6478ea0cf55c53b2fcb8b"
+token="Your Token"
 #keys= curl -u PranavMurali:$token https://api.github.com/users/PranavMurali | jq 'keys'
 #log= curl -u PranavMurali:$token https://api.github.com/users/PranavMurali | jq '.login'
 
