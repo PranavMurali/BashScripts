aff2d11c8cae4049c19afadcd3fad066f07fdc17
Pranav Murali <pranavmurali2001@gmail.com>
2021-02-13 17:13:35 +0530

    :ant: cleaning

--- code/github_api_test.sh
+++ code/github_api_test.sh
@@ -2,13 +2,25 @@
 #!/usr/bin/gnuplot -persist
 url="https://api.github.com/"
 token=""
-
+echo "Your Username?"
+read myname
+echo ""
+echo "Access Token?"
+read token
+echo ""
 names=()
 sizes=()
 upperlim=12
 for ((i=0; i<=${upperlim}; i++)); do
-    name=$(curl -s -u PranavMurali:$token https://api.github.com/users/PranavMurali/repos | jq --argjson q $i '.[$q].name') 
-    size=$(curl -s -u PranavMurali:$token https://api.github.com/users/PranavMurali/repos | jq --argjson q $i '.[$q].size')
+    name=$(curl -s -u PranavMurali:$token https://api.github.com/users/${myname}/repos | jq --argjson q $i '.[$q].name') 
+    size=$(curl -s -u PranavMurali:$token https://api.github.com/users/${myname}/repos | jq --argjson q $i '.[$q].size')
     names+=($name)
     sizes+=($size)
+done
+echo "You have ${#names[@]} repos!" 
+echo ""
+for (( i = 0; i <${#names[@]}; i++ )) 
+do 
+       echo "${names[i]} has size ${sizes[i]} Kb"
+       echo ""
 done
